84b6faa6cdcfcaf5c32dfbcb3505cfff9e85738a
siroinaba <siroinaba@gmail.com>
2025-02-01 23:23:48 +0900

    カメラ挙動修正とコリジョン

--- unity/Hide&Seek/Assets/Scripts/Unit/UnitController.cs
+++ unity/Hide&Seek/Assets/Scripts/Unit/UnitController.cs
@@ -5,11 +5,10 @@ using UnityEngine;
 
 public class UnitController : MonoBehaviour
 {
-    public void Initialize(InGameView view, Transform modelTransform, Camera gameCamera)
+    public void Initialize(InGameView view, Transform modelTransform)
     {
         _view = view;
         _modelTransform = modelTransform;
-        _gameCamera = gameCamera;
     }
 
     public void Excecute()
@@ -29,18 +28,9 @@ public class UnitController : MonoBehaviour
 
         _modelTransform.localRotation = Quaternion.Euler(0.0f, angle, 0.0f);
         _modelTransform.localPosition += _modelTransform.localRotation * new Vector3(0.0f, 0.0f, _speed);
-
-        Vector3 movePos = _modelTransform.localPosition - oldPos;
-        Vector3 cameraPos = _gameCamera.transform.localPosition;
-
-        cameraPos.x += movePos.x;
-        cameraPos.z += movePos.z;
-
-        _gameCamera.transform.localPosition = cameraPos;
     }
 
     private InGameView _view;
     private float _speed = 0.05f;
     private Transform _modelTransform;
-    private Camera _gameCamera;
 }
