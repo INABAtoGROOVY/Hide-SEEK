df896706ea26b1eff8260eabd9122e100f9a79f6
Tofulaboratory <tofulab1373@gmail.com>
2025-02-01 20:03:58 +0900

    update

--- unity/Hide&Seek/Assets/Scripts/GameManager.cs
+++ unity/Hide&Seek/Assets/Scripts/GameManager.cs
@@ -7,14 +7,23 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // TODO  ボタンを押したときの処理を実装する
+        inGameView.Initialize(
+            onClickActionButton: () => Debug.Log("on click action button"),
+            onClickHideButton: () => Debug.Log("on click hide button")
+        );
         StartCoroutine(_sequence.Excecute());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+
+    [SerializeField]
+    private InGameView inGameView;
 
     GameSequence _sequence = new GameSequence();
 }
---
+++ unity/Hide&Seek/Assets/Scripts/View/InGameView.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InGameView : MonoBehaviour
+{
+    public void Initialize(
+        Action onClickActionButton,
+        Action onClickHideButton
+    )
+    {
+        actionButton.OnPointerClickAsObservable().Subscribe(_ =>
+        {
+            onClickActionButton?.Invoke();
+            SetInteractableButton(actionButton, false);
+            Observable.Timer(TimeSpan.FromSeconds(ActionInterval)).Subscribe(_ =>
+            {
+                SetInteractableButton(actionButton, true);
+            }).AddTo(this);
+        }).AddTo(this);
+        hideButton.OnPointerClickAsObservable().Subscribe(_ =>
+        {
+            onClickHideButton?.Invoke();
+        }).AddTo(this);
+    }
+
+    public void SetInteractableHideButton(bool isInteractable) => SetInteractableButton(hideButton, isInteractable);
+
+    public Vector2 GetJoyStickDirection() =>  joystick.Direction;
+
+    private void SetInteractableButton(Button button, bool isInteractable)
+    {
+        button.interactable = isInteractable;
+    }
+
+    [SerializeField]
+    private Button actionButton;
+
+    [SerializeField]
+    private Button hideButton;
+
+    [SerializeField]
+    private Joystick joystick;
+
+    private static readonly float ActionInterval= 3f;
+}
