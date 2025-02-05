874c54ddf6c0aa2a0401636d462713aec727ba01
siroinaba <siroinaba@gmail.com>
2025-02-06 00:14:33 +0900

    色々

--- unity/Hide&Seek/Assets/Scripts/Unit/UnitController.cs
+++ unity/Hide&Seek/Assets/Scripts/Unit/UnitController.cs
@@ -5,17 +5,72 @@ using UnityEngine;
 
 public class UnitController : MonoBehaviour
 {
+    public ControlState controlState{ get{ return _state; } }
+
     public void Initialize(InGameView view, Transform modelTransform)
     {
         _view = view;
         _modelTransform = modelTransform;
+        _state = ControlState.Move;
+
+        _view.Initialize(
+            onClickActionButton: () => ChangeState(ControlState.Avoid),
+            onClickHideButton: () => ChangeState(ControlState.Hide)
+        );
     }
 
     public void Excecute()
     {
-        Debug.Log(_view.GetJoyStick().JoyStickDirection());
+        DebugInput();
 
-        if(_view.GetJoyStick().JoyStickDirection() == Vector2.zero)
+        switch(_state)
+        {
+            case ControlState.Move:
+                Move();
+                break;
+            case ControlState.Avoid:
+                Avoid();
+                break;
+            case ControlState.Hide:
+                Hide();
+                break;
+        }
+
+        _oldState = _state;
+    }
+
+    private void ChangeState(ControlState state)
+    {
+        switch(state)
+        {
+            case ControlState.Move:
+                break;
+            case ControlState.Avoid:
+            case ControlState.Hide:
+                if (_state != ControlState.Move)
+                    return;
+            break;
+        }
+
+        _state = state;
+    }
+
+    private void DebugInput()
+    {
+        if(Input.GetKeyDown(KeyCode.Z))
+        {
+            ChangeState(ControlState.Avoid);
+        }
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            ChangeState(ControlState.Hide);
+        }
+    }
+
+    private void Move()
+    {
+        if (_view.GetJoyStick().JoyStickDirection() == Vector2.zero)
         {
             return;
         }
@@ -30,7 +85,42 @@ public class UnitController : MonoBehaviour
         _modelTransform.localPosition += _modelTransform.localRotation * new Vector3(0.0f, 0.0f, _speed);
     }
 
+    private void Avoid()
+    {
+        if (_oldState != _state)
+        {
+            _avoidTimer = 0.0f;
+        }
+
+        _modelTransform.localPosition += _modelTransform.localRotation * new Vector3(0.0f, 0.0f, 0.5f);
+
+        _avoidTimer += Time.deltaTime;
+
+        if (_avoidTimer > _avoidTime)
+        {
+            _state = ControlState.Move;
+        }
+    }
+
+    private void Hide()
+    {
+        _state = ControlState.Move;
+    }
+
+    public enum ControlState
+    {
+        Move,
+        Avoid,
+        Hide
+    }
+
+    private ControlState _state;
+    private ControlState _oldState;
+
     private InGameView _view;
     private float _speed = 0.05f;
     private Transform _modelTransform;
+
+    private float _avoidTime = 0.1f;
+    private float _avoidTimer = 0.0f;
 }
